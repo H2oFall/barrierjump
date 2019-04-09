f5f1f1561f3896c1b2fe66194b88192a3d75f3f1
H2oFall <42394085+H2oFall@users.noreply.github.com>
2019-04-09 12:50:28 -1000

    Add files via upload

---
+++ plat.js
@@ -0,0 +1,25 @@
+function plat(){
+this.height =50;
+this.width = 200;
+this.x = 700;
+this.y = 300;
+  this.show = function() {
+this.x;
+this.y;
+fill(color('azure'));
+rect(this.x,this.y,this.width,this.height);
+  };
+}
+
+function platform(){
+this.height =50;
+this.width = 200;
+this.x = 100;
+this.y = 400;
+  this.show = function() {
+this.x;
+this.y;
+fill(color('green'));
+rect(this.x,this.y,this.width,this.height);
+  };
+}
